cb0d63571f023cb5f569b0f6ed655bc65356b041
Mikhail Loginov <mikhailloginov93@gmail.com>
2019-12-30 00:43:23 +0300

    make some ideas

--- src/main/java/MultiplicationTable.java
+++ src/main/java/MultiplicationTable.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MultiplicationTable {
+    private String number;
+
+    /**
+     * 1 - long: n - Integer, n < |+- 9 223 372 036 854 775 807|;
+     * 2 - double: a/b =
+     * 3 - bigger then |long|  n > |+- 9 223 372 036 854 775 807|
+     * 4 - complex number: n = z = x + iy
+     * 5 - Fraction: a/b
+     * 6 - with e
+     */
+    private byte typeOfNumber;
+
+    private void setNumber(String nextLine) {
+        this.number = nextLine;
+    }
+
+
+    public static void main(String[] args) {
+        MultiplicationTable myTable = new MultiplicationTable();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number, what ever you want!!!: ");
+        myTable.setNumber(sc.nextLine());
+    }
 }
