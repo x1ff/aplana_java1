27cc1c145ad4e92879fd094f117eb473adf5ed6c
Mikhail Loginov <mikhailloginov93@gmail.com>
2020-01-09 08:44:41 +0300

    update to oop

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,47 +1,55 @@
 import java.util.Scanner;
-
 public class Matrix {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static int length;
+    private static int height;
+    private int [][] arr;
+
+    private void enterDimension(Scanner sc) throws java.util.InputMismatchException {
         System.out.println("Enter length: ");
-        int length;
-        try {
-            length = sc.nextInt();
-        } catch (java.util.InputMismatchException err) {
-            System.out.println("ERR: Length should be integer  " + err);
-            return;
-        }
+        length = sc.nextInt();
         System.out.println("Enter height: ");
-        int height;
-        try {
-            height = sc.nextInt();
-        } catch (java.util.InputMismatchException err) {
-            System.out.println("ERR: height should be integer  " + err);
-            return;
-        }
+        height = sc.nextInt();
         if (height <= 0 | length <= 0) {
             System.out.println("ERR: Dimension should be greater than zero");
             return;
         }
-        int [][] arr = new int[height][length];
+        arr = new int[height][length];
+    }
+    private void enterValues(Scanner sc) throws java.util.InputMismatchException {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < length; j++) {
                 System.out.println("Enter number ");
-                try{
-                    arr[i][j] = sc.nextInt();
-                    System.out.println("Value recorded");
-                } catch (java.util.InputMismatchException e) {
-                    System.out.println("ERR: Value can't recorded: " + e);
-                    return;
-                }
+                arr[i][j] = sc.nextInt();
+                System.out.println("Value recorded");
             }
         }
-        sc.close();
+    }
+    private void printResults() {
         System.out.println("\n Results");
         StringBuilder buff = new StringBuilder();
         for (int j = 0; j < length; j++) {
             buff.append(" ").append(3 * arr[0][j]);
         }
         System.out.println(buff);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Matrix mat = new Matrix();
+        try {
+            mat.enterDimension(sc);
+        } catch (java.util.InputMismatchException err) {
+            System.out.println("ERR: Dimension should be integer  " + err);
+            sc.close();
+            return;
+        }
+        try {
+            mat.enterValues(sc);
+        } catch (java.util.InputMismatchException e) {
+        System.out.println("ERR: Value can't recorded: " + e);
+        sc.close();
+        return;
+        }
+        mat.printResults();
+        sc.close();
     }
 }
